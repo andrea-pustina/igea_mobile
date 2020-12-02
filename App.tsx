570ef55a91da27d61d90fbe739a7126fa4286b24
andrea_pustina <apustin@gmail.com>
2020-12-02 22:02:33 +0100

    tab icon refactor

--- App.tsx
+++ App.tsx
@@ -5,47 +5,67 @@ import OperationsScreen from "./screens/OperationsScreen";
 import PatientsScreen from "./screens/PatientsScreen";
 import SettingScreen from "./screens/SettingScreen";
 import {Icon} from "native-base";
+import {useEffect, useState} from "react";
+import * as Font from 'expo-font';
+import {Ionicons} from '@expo/vector-icons';
+import {AppLoading} from "expo";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+
+    const [isReady, setIsReady] = useState<boolean>(false);
+
+    useEffect(() => {
+        const loadData = async () => {
+            await Font.loadAsync({
+                Roboto: require('native-base/Fonts/Roboto.ttf'),
+                Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+                ...Ionicons.font,
+            });
+
+            setIsReady(true);
+        }
+        loadData();
+    });
+
     return (
-        <NavigationContainer>
-            <Tab.Navigator
-                // tabBar={props => <MyTabBar {...props} />}
-                screenOptions={({route}) => (
-                    {
-                        tabBarIcon: ({focused, color, size}) => {
+        <>
+            {
+                !isReady ? (
+                    <AppLoading/>
+                ) : (
+                    <NavigationContainer>
+                        <Tab.Navigator>
+                            <Tab.Screen name="Operations"
+                                        options={{
+                                            tabBarLabel: "Interventi",
+                                            tabBarIcon: ({focused, color, size}) => {
+                                                return <Icon name={"car"} fontSize={size} style={{color: color}}/>
+                                            }
+                                        }}
+                                        component={OperationsScreen}/>
+                            <Tab.Screen name="Patients"
+                                        options={{
+                                            tabBarLabel: "Pazienti",
+                                            tabBarIcon: ({focused, color, size}) => {
+                                                return <Icon name={"man"} fontSize={size} style={{color: color}}/>
+                                            }
+                                        }}
+                                        component={PatientsScreen}/>
+                            <Tab.Screen name="Settings"
+                                        options={{
+                                            tabBarLabel: "Impostazioni",
+                                            tabBarIcon: ({focused, color, size}) => {
+                                                return <Icon name={"settings"} fontSize={size} style={{color: color}}/>
+                                            }
+                                        }}
+                                        component={SettingScreen}/>
+                        </Tab.Navigator>
+                    </NavigationContainer>
+                )
+            }
+        </>
 
-                            let iconName:string;
-                            switch (route.name){
-                                case "Operations": {
-                                    iconName = "car"
-                                    break;
-                                }
-                                case "Patients": {
-                                    iconName = "man"
-                                    break;
-                                }
-                                case "Settings": {
-                                    iconName = "settings"
-                                    break;
-                                }
-                                default: {
-                                    iconName = "question"
-                                    break;
-                                }
-                            }
-
-                            return <Icon name={iconName} fontSize={size} style={{color:color}} />;
-                        }
-                    }
-                )}
-            >
-                <Tab.Screen name="Operations" component={OperationsScreen}/>
-                <Tab.Screen name="Patients" component={PatientsScreen}/>
-                <Tab.Screen name="Settings" component={SettingScreen}/>
-            </Tab.Navigator>
-        </NavigationContainer>
     );
 }
